6a89bf6fec3b1c898ca756e8e5939a6ec65999dd
tommygun333 <tomas.hlavko@gmail.com>
2026-06-10 15:21:54 +0200

    Fix TV IMDb ratings lookup to use mapped external IDs

--- server/routes/tv.ts
+++ server/routes/tv.ts
@@ -233,7 +233,7 @@ tvRoutes.get('/:id/ratingscombined', async (req, res, next) => {
     const tv = await tmdb.getTvShow({
       tvId: Number(req.params.id),
     });
-    const imdbId = tv.external_ids?.imdb_id ?? null;
+    const imdbId = tv.external_ids?.imdb_id ?? mapTvDetails(tv).externalIds.imdbId;
 
     logger.info('TV ratings combined TMDB lookup', {
       label: 'API',
